4765f564b075a83bc41d35ec0ba4d55722446dd4
Ethan L <ethanlee012345@gmail.com>
2023-03-04 09:02:36 -0600

    Auton rewrite, hopefully works

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -230,6 +230,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return new InstantCommand();
+    return AutonCom.makeAutoCommand(swerve, "Straight");
   }
 }
--- src/main/java/frc/robot/commands/AutonCom.java
+++ src/main/java/frc/robot/commands/AutonCom.java
@@ -23,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.RobotContainer;
 
@@ -75,6 +76,7 @@ public class AutonCom {
     // ));
 
     // eventMap.put("Pick Cube", new InstantCommand(RobotContainer.intake::startIntake, RobotContainer.intakesub));
+
 
 
     
