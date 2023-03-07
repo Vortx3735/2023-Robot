0985bb6e27e244d9df89893238ad552dfca5467f
Ethan L <ethanlee012345@gmail.com>
2023-03-06 18:21:08 -0600

    Adjustable speed

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -4,16 +4,60 @@
 
 package frc.robot.commands;
 
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPlanner;
+
+import java.util.HashMap;
+import java.util.List;
+
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.Constants;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.RobotContainer;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class Auto extends SequentialCommandGroup {
+  List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup("Straight", new PathConstraints(1, 2));
+
+  private static HashMap<String, Command> eventMap = new HashMap<>();
+  
   /** Creates a new Auto. */
-  public Auto() {
+  public Auto(DriveSubsystem drivetrain) {
+
+    
+
+    // SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+    //   drivetrain::getPosition, 
+    //   drivetrain::resetOdometry, 
+    //   Constants.DriveConstants.kDriveKinematics, 
+    //   new PIDConstants(1, 0, 0), //ETHANNNNNNN PID PID PID PID PID ENJOYYYYYY
+    //   new PIDConstants(1, 0, 0), //SAME HEREREEEEEEEE
+    //   drivetrain::setModuleStates, 
+    //   eventMap, 
+    //   true,
+    //   drivetrain
+    // );
+
+    //new SwerveAutoBuilder(null, null, null, null, null, eventMap, null)
+
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands();
+    addCommands(
+      // autoBuilder.fullAuto(pathGroup)
+    );
   }
 }
